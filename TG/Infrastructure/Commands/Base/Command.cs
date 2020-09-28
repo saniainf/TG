b9340b8dbaa37287980fed943bd6d940b40c9f40
sania <saniainf@gmail.com>
2020-09-28 22:54:23 +0700

    Базовый класс команды

---
+++ TG/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TG.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TG/ViewModels/MainWindowViewModel.cs
+++ TG/ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,41 @@ namespace TG.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region заголовок окна
+        #region Title : Заголовок окна
 
         private string _Title = "Работа Типографии";
-        /// <summary>
-        /// Заголовок окна
-        /// </summary>
+
+        /// <summary> Заголовок окна </summary>
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Status : Статус программы
+
+        private string _Status = "Все готово";
+
+        /// <summary> Статус программы </summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        #endregion
+
+        #region Progress : Прогресс выполнения
+
+        private double _Progress;
+
+        /// <summary> Прогресс выполнения </summary>
+        public double Progress
+        {
+            get => _Progress;
+            set => Set(ref _Progress, value);
         }
 
         #endregion
