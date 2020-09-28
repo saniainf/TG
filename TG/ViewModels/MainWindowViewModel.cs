a86fec116da6b2ba4a21cbbc176061307aa3b665
sania <saniainf@gmail.com>
2020-09-28 18:21:46 +0700

    Тестирование работы view-model

---
+++ TG/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using TG.ViewModels.Base;
+
+namespace TG.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region заголовок окна
+
+        private string _Title = "Работа Типографии";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
