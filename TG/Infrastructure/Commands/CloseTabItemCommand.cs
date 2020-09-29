d7d86b95b8e56a31e6ef29398c73884882267ee8
sania <saniainf@gmail.com>
2020-09-29 23:09:55 +0700

    Иконки. Команда закрытия вкладки.

---
+++ TG/Infrastructure/Commands/CloseTabItemCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using TG.Infrastructure.Commands.Base;
+
+namespace TG.Infrastructure.Commands
+{
+    class CloseTabItemCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            TabControl panel = (TabControl)parameter;
+            panel.Items.Remove(panel.SelectedItem);
+        }
+    }
+}
