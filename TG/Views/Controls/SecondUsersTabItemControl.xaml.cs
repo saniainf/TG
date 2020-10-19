607ce6fc060f310e07fdd7cdb3fa07cb1730d999
sania <saniainf@gmail.com>
2020-10-19 20:07:29 +0700

    Добавил и настроил виртуализацию списка отделов

---
+++ TG/Views/Controls/SecondUsersTabItemControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using TG.Models.Company;
+
+namespace TG.Views.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondUsersTabItemControl.xaml
+    /// </summary>
+    public partial class SecondUsersTabItemControl : UserControl
+    {
+        public SecondUsersTabItemControl()
+        {
+            InitializeComponent();
+        }
+
+        private void DepartmentCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Department department)) return;
+            if (department.Name is null) return;
+
+            var filterText = DepartmentNameFilterText.Text;
+            if (string.IsNullOrEmpty(filterText)) return;
+
+            if (department.Name.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+            if (department.Description != null && department.Description.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnDepartmentFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            var collection = (CollectionViewSource)textBox.FindResource("DepartmentsCollection");
+            collection.View.Refresh();
+        }
+    }
+}
