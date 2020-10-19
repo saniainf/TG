e477f621d44852ab7b6d7b7454d9a5022198e69a
sania <saniainf@gmail.com>
2020-10-19 11:42:27 +0700

    Разделение TabControl на разные TabItem

---
+++ TG/Views/Controls/TestTabItemControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TG.Views.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TestTabItemControl.xaml
+    /// </summary>
+    public partial class TestTabItemControl : UserControl
+    {
+        public TestTabItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
